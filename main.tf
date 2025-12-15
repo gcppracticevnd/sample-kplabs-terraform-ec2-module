e0caeb9924178893ecc181cfffb923035a3bd7bf
gcppracticevnd <gcppracticevnd@gmail.com>
2025-12-15 21:37:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
-provider "aws" {
-  //region = "ap-south-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.26.0"
+    }
+  }
 }
 
 resource "aws_instance" "myec2" {
     ami = "ami-02d26659fd82cf299"
     instance_type = "t3.micro"
 }
+
+
