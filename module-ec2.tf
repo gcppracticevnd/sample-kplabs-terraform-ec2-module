016c85a2fb7a6ddfbdfe34066613ed514df52a37
gcppracticevnd <gcppracticevnd@gmail.com>
2025-12-16 01:13:58 +0530

    Update module-ec2.tf

--- module-ec2.tf
+++ module-ec2.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.26.0"
-    }
-  }
-}
-
-provider "aws"{
-  region = var.region
-}
-
 resource "aws_instance" "myec2" {
     ami = var.ami
     instance_type = var.instance_type
